beda832298d32351eb6ae0e5b62b6584f09ec2cf
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2023-09-24 00:07:53 +0530

    fix: changed entrypoint to watch_reload.sh

--- nginx/watch_reload.sh
+++ nginx/watch_reload.sh
@@ -3,26 +3,32 @@
 
 # Thanks to https://cyral.com/blog/how-to-auto-reload-nginx/
 
-if [[ $(command -v inotifywait) != "" ]];
-then
-	while true
-	do
-		inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/sites-enabled/
+watch_reloads () {
+	if [[ $(command -v inotifywait) != "" ]];
+	then
+		while true
+		do
+			inotifywait --exclude .swp -e create -e modify -e delete -e move /etc/nginx/sites-enabled/
 
-		echo "Detected Nginx configuration change."
+			echo "Detected Nginx configuration change."
 
-		# Tests the new configuration
-		nginx -t
+			# Tests the new configuration
+			nginx -t
 
-		if [ $? -eq 0 ]
-		then
-			echo "Configuration OK. Executing: nginx -s reload"
-			nginx -s reload
-		else
-			echo "Error in configuration. Did not reload Nginx."
-		fi
-	done
-else
-	echo "Inotifywait not found. Configuration reloads cannot be done."
-	exit 1
-fi
+			if [ $? -eq 0 ]
+			then
+				echo "Configuration OK. Executing: nginx -s reload"
+				nginx -s reload
+			else
+				echo "Error in configuration. Did not reload Nginx."
+			fi
+		done
+	else
+		echo "Inotifywait not found. Configuration reloads cannot be done."
+		exit 1
+	fi
+}
+
+watch_reloads &
+
+nginx -g 'daemon off;'
